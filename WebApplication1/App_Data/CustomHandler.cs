0cebc3cc664095a07a0fabcbb73a45820731d1dc
Castorg <rumata.it@gmail.com>
2015-02-15 15:32:06 +0300

    create simple CustomHandler.cs.

---
+++ WebApplication1/App_Data/CustomHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WebApplication1.App_Data
+{
+    public class CustomHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            const String htmlTemplate = "<html><head><title>{0}</title></head><body>" +
+            "<h1>Hello I'm: " +
+            "<span style='color:blue'>{1}</span></h1>" +
+            "</body></html>";
+            var response = String.Format(htmlTemplate,
+            "HTTP Handlers", context.Request.Path);
+            context.Response.Write(response);
+        }
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+
+    }
+}
